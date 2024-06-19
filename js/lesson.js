e0f5d8b96873a504eaa9d18bf43800defac087b9
JrnJ <jeroenjochems@outlook.com>
2024-06-19 15:58:02 +0200

    quick change

--- js/lesson.js
+++ js/lesson.js
@@ -48,36 +48,30 @@ let AllLessons = [];
 let JSON_LESSON = [];
 
 function GetAllLessons() {
-    AllLessons.push(JSON.parse(JP_EN_HiraganaFile));
-    AllLessons.push(JSON.parse(JP_EN_HiraganaCombination));
-    AllLessons.push(JSON.parse(JP_EN_NumbersFile));
-    AllLessons.push(JSON.parse(JP_EN_ColorsFile));
-    AllLessons.push(JSON.parse(JP_EN_EverydayObjects));
-    AllLessons.push(JSON.parse(ITIL_GEN));
-    AllLessons.push(JSON.parse(ITIL_DOEL));
-    AllLessons.push(JSON.parse(ITIL_ALL));
-    AllLessons.push(JSON.parse(ITIL_H4));
-    AllLessons.push(JSON.parse(SECURITY));
-    AllLessons.push(JSON.parse(SEC_ALGORTIME));
-    AllLessons.push(JSON.parse(NET_BEGRIPPEN));
-    AllLessons.push(JSON.parse(NET_BOEK));
+    // AllLessons.push(JSON.parse(JP_EN_HiraganaFile));
+    // AllLessons.push(JSON.parse(JP_EN_HiraganaCombination));
+    // AllLessons.push(JSON.parse(JP_EN_NumbersFile));
+    // AllLessons.push(JSON.parse(JP_EN_ColorsFile));
+    // AllLessons.push(JSON.parse(JP_EN_EverydayObjects));
+    // AllLessons.push(JSON.parse(ITIL_GEN));
+    // AllLessons.push(JSON.parse(ITIL_DOEL));
+    // AllLessons.push(JSON.parse(ITIL_ALL));
+    // AllLessons.push(JSON.parse(ITIL_H4));
+    AllLessons.push(JSON.parse(ITIL));
 }
 
 const GetLessonByName = (name) => {
     switch (name) {
-        case "Any": return JSON.parse(JSON_LESSON);
-        case "Hiragana": return JSON.parse(JP_EN_HiraganaFile);
-        case "Hiragana Combination": return JSON.parse(JP_EN_HiraganaCombination);
-        case "Numbers": return JSON.parse(JP_EN_NumbersFile);
-        case "Colors": return JSON.parse(JP_EN_ColorsFile);
-        case "Everyday Objects": return JSON.parse(JP_EN_EverydayObjects);
-        case "ITIL_GEN": return JSON.parse(ITIL_GEN);
-        case "ITIL_DOEL": return JSON.parse(ITIL_DOEL);
-        case "ITIL_ALL": return JSON.parse(ITIL_ALL);
-        case "ITIL-H4": return JSON.parse(ITIL_H4);
-        case "SECURITY": return JSON.parse(SECURITY);
-        case "SEC | Algoritme": return JSON.parse(SEC_ALGORTIME);
-        case "Netwerken | Begrippen": return JSON.parse(NET_BEGRIPPEN);
-        case "Netwerken | Boek": return JSON.parse(NET_BOEK);
+        // case "Any": return JSON.parse(JSON_LESSON);
+        // case "Hiragana": return JSON.parse(JP_EN_HiraganaFile);
+        // case "Hiragana Combination": return JSON.parse(JP_EN_HiraganaCombination);
+        // case "Numbers": return JSON.parse(JP_EN_NumbersFile);
+        // case "Colors": return JSON.parse(JP_EN_ColorsFile);
+        // case "Everyday Objects": return JSON.parse(JP_EN_EverydayObjects);
+        // case "ITIL_GEN": return JSON.parse(ITIL_GEN);
+        // case "ITIL_DOEL": return JSON.parse(ITIL_DOEL);
+        // case "ITIL_ALL": return JSON.parse(ITIL_ALL);
+        // case "ITIL-H4": return JSON.parse(ITIL_H4);
+        case "ITIL": return JSON.parse(ITIL);
     }
 }
